383ea24d71923e668a25158cec9d3da81e59e408
alperenkucuktag <alperenkucuktag26.97@gmail.com>
2023-10-23 14:01:51 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# MyLittle-Tour-App
---
+++ main.js
@@ -0,0 +1,11 @@
+const menuBtn=document.querySelector('.navbar-menu-btn')
+//console.log(menuBtn)
+const menuLinks=document.querySelector('.navbar-links')
+//console.log(menuLinks)
+menuBtn.addEventListener('click',()=>{
+    menuLinks.classList.toggle('mobile-menu')
+}
+)
+
+
+
